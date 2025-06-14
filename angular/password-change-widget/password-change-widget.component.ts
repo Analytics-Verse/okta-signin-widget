a5a9893209571f5b1121d9ffc50633d1d4661669
shharrnam <shharrnam@analyticsverse.com>
2025-06-14 19:05:11 +0530

    feat: add Angular password change widget example

---
+++ angular/password-change-widget/password-change-widget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-change-widget',
+  templateUrl: './password-change-widget.component.html',
+  styleUrls: ['./password-change-widget.component.scss']
+})
+export class PasswordChangeWidgetComponent {
+  // Purely presentational component, no API calls
+}
